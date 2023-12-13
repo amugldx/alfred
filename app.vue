d9774103bea122fc9bd955d3df871826c88a90a2
amrmugl <meet.sagi.here@gmail.com>
2023-12-13 21:43:41 +0500

    app done

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtPage />
+		<UNotifications />
+		<SubscribeModal />
+	</div>
 </template>
+
+<style>
+	body {
+		@apply bg-gray-100 dark:bg-gray-900;
+	}
+</style>
